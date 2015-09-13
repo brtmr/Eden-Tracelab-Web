04378848c548fbaf7bc605eab6679244b2373ea6
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-09-13 18:09:25 +0200

    added sql statements for retrieving events

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 {- Backend for retrieving trace data from the database. -}
+import Bachelor.Types
 import Control.Monad
 import Control.Monad.Trans (liftIO)
 import Data.Monoid (mconcat)
@@ -21,13 +22,18 @@ myConnectInfo = defaultConnectInfo {
     }
 
 main = do
-
     conn <- connect myConnectInfo
-
     scotty 3000 $ do
         middleware logStdoutDev
         post "/traces" $ do
             traceListAction conn
+        post "/events" $ do
+            id       <- param "id"
+            t        <- param "type"
+            start    <- param "start"
+            end      <- param "end"
+            minduration <- param "minduration"
+            getEvents conn id t start end minduration
 
 {-
  - ## TRACEINFO SECTION ##
@@ -58,5 +64,20 @@ getTraceList :: Connection -> IO [(Int, Text.Text, LocalTimestamp)]
 getTraceList conn = query_ conn [sql|SELECT trace_id,filename,creation_date FROM TRACES|]
 
 {-
- - ## MACHINEEVENT SECTION
+ - ## EVENT RETRIEVAL SECTION
  -}
+getEvents :: Connection -> Int -> String -> Int -> Int -> Int -> ActionM()
+getEvents conn id t start end minduration = do
+    text $ "works."
+
+getMachineEvents :: Connection -> Int -> Int -> Int -> Int ->
+    IO [(Int,Int,Int,Int)]
+getMachineEvents conn trace_id start end minduration = do
+    evs <- query conn [sql|SELECT * FROM MACHINE_EVENTS
+        WHERE STARTTIME > ?
+        AND   DURATION  > ?
+        and   STARTTIME < ?
+        AND   MACHINE_ID in
+        (SELECT MACHINE_ID FROM MACHINES WHERE
+        TRACE_ID = ?)|] (start, minduration, end, trace_id)
+    return evs
