0169108877a8e1b61654ba3c5afa4abdf75777a8
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-09-13 16:38:54 +0200

    TraceList works.

--- Main.hs
+++ Main.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 {- Backend for retrieving trace data from the database. -}
-import Database.PostgreSQL.Simple.Time
+import Control.Monad
+import Control.Monad.Trans (liftIO)
 import Data.Monoid (mconcat)
-import qualified Data.Text as Text
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.SqlQQ
+import Database.PostgreSQL.Simple.Time
 import Network.Wai.Middleware.RequestLogger
-import Control.Monad
 import Web.Scotty
+import qualified Data.Text as Text
+
+import Data.Aeson.Types (ToJSON, (.=), object, toJSON)
 
 myConnectInfo :: ConnectInfo
 myConnectInfo = defaultConnectInfo {
@@ -23,11 +27,36 @@ main = do
     scotty 3000 $ do
         middleware logStdoutDev
         post "/traces" $ do
-            json $ [("file1" :: String), "file2", "file3" ]
+            traceListAction conn
 
-getTraceList :: Connection -> IO()
-getTraceList conn = do
-    xs <- query_ conn "SELECT filename,creation_date FROM TRACES"
-    forM_ xs $ \(name, date) -> do
-        print $ Text.unpack (name :: Text.Text)
-        print $ (date :: LocalTimestamp)
+{-
+ - ## TRACEINFO SECTION ##
+ -}
+data TraceInfo = TraceInfo {
+    id :: Int,
+    filename :: String,
+    date :: String
+    } deriving (Show)
+
+instance ToJSON TraceInfo where
+    toJSON (TraceInfo id fn date) =
+        object [
+            "id"       .= id,
+            "filename" .= fn,
+            "date"     .= date
+        ]
+--Scotty Action
+traceListAction :: Connection -> ActionM()
+traceListAction conn = do
+    traceList <- liftIO $ fmap
+        (map (\(id,filename,date) ->
+            TraceInfo id (Text.unpack filename) (show date))) $ getTraceList conn
+    json $ traceList
+
+--returns a list of traces, so that the user can pick which one to examine.
+getTraceList :: Connection -> IO [(Int, Text.Text, LocalTimestamp)]
+getTraceList conn = query_ conn [sql|SELECT trace_id,filename,creation_date FROM TRACES|]
+
+{-
+ - ## MACHINEEVENT SECTION
+ -}
