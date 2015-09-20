b69063ee836afcb35ee5ff1a96619fd53af03e95
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-09-20 14:19:08 +0200

    /traceinfo and /mevents works!

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@
 import Bachelor.Types
 import Control.Monad
 import Control.Monad.Trans (liftIO)
+import Data.List
 import Data.Monoid (mconcat)
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.SqlQQ
@@ -15,6 +16,11 @@ import Web.Scotty
 import qualified Data.Text as Text
 
 import Data.Aeson.Types (ToJSON, (.=), object, toJSON)
+
+type TraceId   = Int
+type ThreadId  = Int
+type MachineId = Int
+type ProcessId = Int
 
 myConnectInfo :: ConnectInfo
 myConnectInfo = defaultConnectInfo {
@@ -27,25 +33,29 @@ main = do
         middleware logStdoutDev
         post "/traces" $ do
             traceListAction conn
-        post "/events" $ do
+        post "/mevents" $ do
             id       <- param "id"
-            t        <- param "type"
             start    <- param "start"
             end      <- param "end"
             minduration <- param "minduration"
-            getEvents conn id t start end minduration
+            evs <- liftIO $ getMachineEvents conn id start end minduration
+            json $ evs
+        post "/traceinfo" $ do
+            id <- param "id"
+            traceInfo <- liftIO $ getTraceInfo conn id
+            json $ traceInfo
 
 {-
  - ## TRACEINFO SECTION ##
  -}
-data TraceInfo = TraceInfo {
+data TraceEntry = TraceEntry {
     id :: Int,
     filename :: String,
     date :: String
     } deriving (Show)
 
-instance ToJSON TraceInfo where
-    toJSON (TraceInfo id fn date) =
+instance ToJSON TraceEntry where
+    toJSON (TraceEntry id fn date) =
         object [
             "id"       .= id,
             "filename" .= fn,
@@ -56,7 +66,7 @@ traceListAction :: Connection -> ActionM()
 traceListAction conn = do
     traceList <- liftIO $ fmap
         (map (\(id,filename,date) ->
-            TraceInfo id (Text.unpack filename) (show date))) $ getTraceList conn
+            TraceEntry id (Text.unpack filename) (show date))) $ getTraceList conn
     json $ traceList
 
 --returns a list of traces, so that the user can pick which one to examine.
@@ -66,18 +76,50 @@ getTraceList conn = query_ conn [sql|SELECT trace_id,filename,creation_date FROM
 {-
  - ## EVENT RETRIEVAL SECTION
  -}
-getEvents :: Connection -> Int -> String -> Int -> Int -> Int -> ActionM()
-getEvents conn id t start end minduration = do
-    text $ "works."
-
 getMachineEvents :: Connection -> Int -> Int -> Int -> Int ->
     IO [(Int,Int,Int,Int)]
 getMachineEvents conn trace_id start end minduration = do
-    evs <- query conn [sql|SELECT * FROM MACHINE_EVENTS
-        WHERE STARTTIME > ?
-        AND   DURATION  > ?
-        and   STARTTIME < ?
-        AND   MACHINE_ID in
-        (SELECT MACHINE_ID FROM MACHINES WHERE
-        TRACE_ID = ?)|] (start, minduration, end, trace_id)
+    evs <- query conn [sql|SELECT NUM, STARTTIME, DURATION, STATE FROM (MACHINE_EVENTS JOIN MACHINES
+    ON MACHINE_EVENTS.MACHINE_ID = MACHINES.MACHINE_ID)
+    WHERE STARTTIME > ?
+    AND   DURATION  > ?
+    AND   STARTTIME < ?
+    AND   MACHINES.MACHINE_ID in
+    (SELECT MACHINE_ID FROM MACHINES WHERE
+    TRACE_ID = ?)|] (start, minduration, end, trace_id)
     return evs
+
+{-
+ - EVENT INFO SECTION
+ - returns the entire trace structure as a tree, without any events.
+ -}
+type TraceInfo = [(MachineId,[(ProcessId,[ThreadId])])]
+
+getTraceInfo :: Connection -> TraceId -> IO TraceInfo
+getTraceInfo conn traceId = do
+    res <- query conn [sql|SELECT P.MACHINE_NUM, P.PROCESS_NUM, NUM as THREAD_NUM FROM
+        THREADS
+        JOIN (SELECT M.MACHINE_NUM, PROCESS_ID, NUM AS PROCESS_NUM from
+            PROCESSES
+            JOIN (SELECT MACHINES.MACHINE_ID, MACHINES.NUM as MACHINE_NUM FROM
+                (SELECT * FROM TRACES WHERE TRACE_ID = ?) T
+                JOIN MACHINES
+                ON T.TRACE_ID = MACHINES.TRACE_ID) M
+            ON M.MACHINE_ID = PROCESSES.MACHINE_ID) P
+        ON P.PROCESS_ID = THREADS.PROCESS_ID|] $ Only traceId
+    return $ mkTraceInfo res
+
+--takes a flattened trace structure, and returns a hierarchical one.
+mkTraceInfo :: [(MachineId, ProcessId, ThreadId)] -> TraceInfo
+mkTraceInfo xs =
+    map (\mid -> (mid,map (\(m,p,ts) -> (p,ts))
+        $ filter (\(m,p,ts) -> m==mid) threads)) $ firstone xs
+    where
+        threads = map (\(m,p) -> (m,p,(map (\(_,_,x) -> x) $
+            filter (\(m',p',_) -> (m',p') == (m,p)) xs))) $ firsttwo xs
+
+firsttwo :: Eq a => [(a,a,a)] -> [(a,a)]
+firsttwo xs = nub $ map (\(a,b,c) -> (a,b)) xs
+
+firstone :: Eq a => [(a,a,a)] -> [a]
+firstone xs = nub $ map (\(a,b,c) -> a) xs
